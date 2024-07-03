69c60c4db5bd388dfd900fa8c288fbe66e3451e9
Alexandre Jezus <alexandrejezus.p@gmail.com>
2024-07-03 11:07:52 -0300

    func

--- funcao.js
+++ funcao.js
@@ -1,42 +1,32 @@
 const opcao = [1, 2, 3, 4, 5];
-if (opcao == 1) {
-  console.log("teste");
-  return;
-}
 function soma(a, b) {
-  if (typeof a !== Number || typeof b !== Number) {
-    console.log("Os parâmetros devem ser números.");
-  } else somaResultado = a + b;
-  console.log("O resultado da soma é: " + somaResultado);
-  return;
+  let somaResultado = a + b;
+  console.log("O resultado da soma é: ");
+  return somaResultado;
 }
 
 function subtracao(a, b) {
-  if (typeof a !== Number || typeof b !== Number) {
-    console.log("Os parâmetros devem ser números.");
-  } else subtracaoResultado = a - b;
-  return;
+  let subtracaoResultado = a - b;
+  console.log("O resultado da subtração é: ");
+  return subtracaoResultado;
 }
 
-function multiplicacao(a, f) {
-  if (typeof a !== Number || typeof b !== Number) {
-    console.log("Os parâmetros devem ser números.");
-  } else multiplicacaoResultado = a * f;
-  return;
+function multiplicacao(a, b) {
+  let multiplicacaoResultado = a * b;
+  console.log("O resultado da multiplicação é: ");
+  return multiplicacaoResultado;
 }
 
 function divisao(a, b) {
-  if (typeof a !== Number || typeof b !== Number) {
-    console.log("Os parâmetros devem ser números.");
-  } else divisaoResultado = a / b;
-  return;
+  let divisaoResultado = a / b;
+  console.log("O resultado da divisão é: ");
+  return divisaoResultado;
 }
 
 function porcentagem(a, b) {
-  if (typeof a !== Number || typeof b !== Number) {
-    console.log("Os parâmetros devem ser números.");
-  } else porcentagemResultado = (a / 100) * b;
-  return;
+  let porcentagemResultado = (a / 100) * b;
+  console.log("O resultado da porcentagem é: ");
+  return porcentagemResultado;
 }
 
 module.exports = {
